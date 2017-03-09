9df0958303b8cdd24b155bd75b16ae9bdebbfc46
maximebourdel <bourdel.maxime.pro@gmail.com>
2017-03-10 00:03:51 +0100

    adding missing rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// Add the RxJS Observable operators.
+import './rxjs-operators';
+
 @Component({
     selector: 'my-app',
     templateUrl: 'app.component.html',
